d736cc77fd4df78fb39a50a92adaf264b8633a2a
Lenin O <o.lamprea99@gmail.com>
2025-09-27 22:29:33 -0500

    cors back added

--- back/up-study-back/src/main.ts
+++ back/up-study-back/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ Activar CORS
+  app.enableCors({
+    origin: '*', // dominio de tu frontend Angular
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,               // si envías cookies o headers de auth
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
